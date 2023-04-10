dcb8d38f073ffa55395360489e64087fe2e177e7
Анищенко Никита Викторович <nvanishenko@element-lab.ru>
2023-04-10 19:45:23 +0400

    #Test Multithreadin...

--- TestMultithreading/src/main/java/ru/ibs/testmultithreading/TestMultithreadingStart.java
+++ TestMultithreading/src/main/java/ru/ibs/testmultithreading/TestMultithreadingStart.java
@@ -3,6 +3,7 @@ package ru.ibs.testmultithreading;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import ru.ibs.sortings.BucketSort;
+import ru.ibs.sortings.RadixSort;
 
 public class TestMultithreadingStart {
 
@@ -14,8 +15,8 @@ public class TestMultithreadingStart {
 //		new QuickSort().quickSort(arr, 0, arr.length - 1);
 //		int[] arr = new int[]{2, 24, 45, 66, 75, 90, 170, 802};
 //		int[] arr = new int[]{75, 98, 174, 802};
-//		int[] arr = new int[]{802, 178, 55, 22};
-//		new RadixSort().radixSort(arr, arr.length);
+		int[] arr2 = new int[]{802, 178, 55, 22};
+		new RadixSort().radixSort(arr2, arr2.length);
 		int[] arr = new int[]{25, 30, 28, 22};
 //		new CountingSort().countSort(arr);
 //		float[] arr = {(float) 0.42, (float) 0.32, (float) 0.33, (float) 0.52, (float) 0.37, (float) 0.47, (float) 0.51};
